fbbabd25a48c79d8421433862245e1e3d76c1597
kumar2056 <42170484+kumar2056@users.noreply.github.com>
2019-01-14 19:06:00 +0530

    Create alpha.cpp

---
+++ alpha.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+char a;
+cout<<"enter the char";
+cin>>a;
+if((a>='a'&&a<='z')||(a>='A'&&a<='Z'))
+{
+cout<<"alphabet";
+}
+else
+{
+cout>>"no";
+}
+return 0;
+}
